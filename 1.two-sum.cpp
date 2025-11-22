7bfe346acc42dc8ac69f20af13b42debe7ba045e
Jack Nguyen <J4ck.tf@gmail.com>
2025-11-22 17:06:41 -0500

    two sum

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -7,8 +7,16 @@
 // @lc code=start
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> map;
+        for (int i=0; i<nums.size(); i++) {
+            auto j = map.find(target - nums[i]);
+            if (j != map.end()) {
+                return {i, j->second};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
     }
 };
 // @lc code=end
